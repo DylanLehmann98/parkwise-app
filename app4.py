d016a7f0a5bda1a803e19b4ff459baf71f0ddfd9
DylanLehmann98 <dylan.lehmann@me.com>
2025-07-20 14:06:12 +0000

    Initial commit

---
+++ app4.py
@@ -0,0 +1,9 @@
+cd ~/Desktop/Parkwise_SevenDials
+git init
+git add .
+git commit -m "Initial commit"
+git branch -M main
+git remote add origin https://github.com/Dylanlehmann98/parkwise-app.git
+git push -u origin main
+
+ 
